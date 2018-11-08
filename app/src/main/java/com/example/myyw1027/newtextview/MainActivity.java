add7cd6906441620e2740b0a3e7fa344dac27e04
myyw1027 <myyw1027@sina.com>
2018-11-08 14:41:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myyw1027/newtextview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myyw1027.newtextview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mtextview.YtdUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        YtdUtils.fdfds();
+        YtdUtils.fdfds();
+    }
+}
